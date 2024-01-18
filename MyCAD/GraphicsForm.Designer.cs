66eb02afe9a6e7857735a51f54f5df2977075732
ToLeThanh <hanhbt27052019@gmail.com>
2024-01-18 23:33:49 +0700

    Add project files.

---
+++ MyCAD/GraphicsForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace MyCAD
+{
+    partial class GraphicsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawing = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.drawing)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawing
+            // 
+            this.drawing.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.drawing.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.drawing.Location = new System.Drawing.Point(0, 0);
+            this.drawing.Name = "drawing";
+            this.drawing.Size = new System.Drawing.Size(1074, 512);
+            this.drawing.TabIndex = 0;
+            this.drawing.TabStop = false;
+            this.drawing.MouseMove += new System.Windows.Forms.MouseEventHandler(this.drawing_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 533);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 572);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // GraphicsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1257, 627);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.drawing);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "GraphicsForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.drawing)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox drawing;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MyCAD/GraphicsForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCAD
+{
+    public partial class GraphicsForm : Form
+    {
+        public GraphicsForm()
+        {
+            InitializeComponent();
+        }
+        private PointF currentPosition;
+        private void drawing_MouseMove(object sender, MouseEventArgs e)
+        {
+            currentPosition = PointToCartesian(e.Location);
+            label1.Text = string.Format("{0}, {1}", e.Location.X, e.Location.Y);
+            label2.Text = string.Format("{0,0:F3}, {1,0:F3}", currentPosition.X, currentPosition.Y);
+        }
+
+        //Convert system point to world point
+        private PointF PointToCartesian(Point point)
+        {
+            return new PointF(Pixel_to_Mn(point.X), Pixel_to_Mn(drawing.Height - point.Y));
+        }
+
+        //Get screen dpi
+        private float DPI
+        {
+            get
+            {
+                using(var g = CreateGraphics())
+                {
+                    return g.DpiX;
+                }
+            }
+        }
+        //Convert pixels to millimeters
+        private float Pixel_to_Mn(float pixel)
+        {
+            return pixel * 25.4f / DPI;
+        }
+    }
+}
